42a28b6cd31374e5bb74922e6a31c85919b3ab2c
Jacob Ranz <jacobranz4@gmail.com>
2022-11-01 21:30:49 -0700

    Adding student logic

--- test_database_build.py
+++ test_database_build.py
@@ -3,23 +3,24 @@ import mysql.connector
 from numpy import append
 import pandas as pd
 
-#mydb = mysql.connector.connect(
-#    host = "127.0.0.1",
-#    user = "root",
-#    password = "1234",
-#    auth_plugin='mysql_native_password',
-#    database = "test"
-#)
+mydb = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    #password = "1234", ## My databse currently has no password
+    auth_plugin='mysql_native_password',
+    database = "SCHOOL_MANAGEMENT"
+)
 
-#cursor = mydb.cursor()
+cursor = mydb.cursor()
 
-#cursor.execute("CREATE TABLE IF NOT EXISTS student (student_ID INT primary key auto_increment, first_name CHAR(255), last_name CHAR(255), courses VARCHAR(255), grade_level VARCHAR(255), age SMALLINT)")
-#cursor.execute("CREATE TABLE IF NOT EXISTS professor (professor_ID INT, first_name CHAR(255), last_name CHAR(255), classes VARCHAR(255), class_count SMALLINT, start_date DATE)")
-#cursor.execute("CREATE TABLE IF NOT EXISTS course (course_ID INT, name VARCHAR(255), professor_first CHAR(255), professor_last CHAR(255), student_first CHAR(255), student_last CHAR(255), grade_level CHAR(255))")
-#cursor.execute("CREATE TABLE IF NOT EXISTS student_course (student_ID INT references student(student_ID), course_ID INT references course(course_ID))")
-#cursor.execute("CREATE TABLE IF NOT EXISTS professor_course (professor_ID INT references professor(professor_ID), course_ID INT references course(course_ID))")
-#cursor.execute("CREATE TABLE IF NOT EXISTS student_course_grade (student_ID INT references student(student_ID), class_ID INT references class(class_ID), assignment_grade FLOAT(24), weight FLOAT(24))")
-#mydb.commit()
+## Create database tables based on current use cases
+cursor.execute("CREATE TABLE IF NOT EXISTS student (student_ID INT primary key auto_increment, first_name CHAR(255), last_name CHAR(255), courses VARCHAR(255), grade_level VARCHAR(255), age SMALLINT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS professor (professor_ID INT, first_name CHAR(255), last_name CHAR(255), classes VARCHAR(255), class_count SMALLINT, start_date DATE)")
+cursor.execute("CREATE TABLE IF NOT EXISTS course (course_ID INT, name VARCHAR(255), professor_first CHAR(255), professor_last CHAR(255), student_first CHAR(255), student_last CHAR(255), grade_level CHAR(255))")
+cursor.execute("CREATE TABLE IF NOT EXISTS student_course (student_ID INT references student(student_ID), course_ID INT references course(course_ID))")
+cursor.execute("CREATE TABLE IF NOT EXISTS professor_course (professor_ID INT references professor(professor_ID), course_ID INT references course(course_ID))")
+cursor.execute("CREATE TABLE IF NOT EXISTS student_course_grade (student_ID INT references student(student_ID), class_ID INT references class(class_ID), assignment_grade FLOAT(24), weight FLOAT(24))")
+mydb.commit()
 
 class Person:
     def __init__(self, first_name, last_name, age):
@@ -27,11 +28,14 @@ class Person:
         self.last_name = last_name
         self.age = age
         
-class Student(Person):
+class Student(Person): # Inherited from Person class
+
+    ## Button correlates to each one of these functions
+    ## At the end of the class, all of the data will be entered into the database
     def __init__(self):
         self.courses = []
         
-    def set_first_name(self, first_name):
+    def set_first_name(self, first_name): ## When entering student first name in GUI, the button runs this command 
         self.first_name = first_name
     
     def set_last_name(self, last_name):
@@ -44,18 +48,29 @@ class Student(Person):
         
     def set_grade_level(self, grade_level):
         self.grade_level = grade_level
-        #cursor.execute("INSERT INTO student grade_level VALUES (%s)", self.grade_level)
 
-    def get_courses(self): #### Not printing values
+    def get_courses(self): #### Not printing values - need to fix
         for course in self.courses:
             print(course)
-    
-    def get_grade_level(self):
-        cursor.execute("SELECT grade_level FROM student WHERE student_ID = 1")
-        for x in cursor.fetchall():
-            print(x)
-    
+
+    def write_database(self):
+        cursor.execute("""
+        INSERT INTO student (first_name, last_name, classes, grade_level, age)
+        VALUES (%s,%s,%s,%s,%s,%s)
+        """, (self.first_name , self.last_name, self.courses, self.grade_level, self.age))
+        mydb.commit()
+
+class Professor(Person):
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+
+    def set_courses(self, courses):
+        pass
+
     
 s = Student()
-s.set_courses("History")
-s.get_courses
+s.set_courses("History, Math, Science")
+s.get_courses()
