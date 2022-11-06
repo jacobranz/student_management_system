d652cd88e2df9efabb71ce63e91089f2423439d6
Jacob Ranz <jacobranz4@gmail.com>
2022-11-06 15:05:37 -0800

    filled out studentadd class

--- FirstProject.py
+++ FirstProject.py
@@ -14,34 +14,32 @@ class studentAdd:
         root.geometry("450x300")
         root.title('Student Management System')
 
-        self.id = test_database_build.Student.set_id()
-        self.first = tk.StringVar()
-        self.last = tk.StringVar()
-        self.grade = tk.IntVar()
+        self.id_var = test_database_build.Student.set_id()
+        self.first_var = tk.StringVar()
+        self.last_var = tk.StringVar()
+        self.age_var = tk.IntVar()
+        self.grade_var = tk.IntVar()
 
-        self.display = tk.Label(root, text = "First Name").place(x=40, y=60)
+        self.first_label = tk.Label(root, text = "First Name").place(x=40, y=80)
+        self.last_label = tk.Label(root, text = "Last Name").place(x=40,y=100)
+        self.age_label = tk.Label(root, text = "Age").place(x=40,y=120)
+        self.grade_label = tk.Label(root, text = "Grade Level").place(x=40,y=140)
 
-        self.last_name = tk.Label(root, text = "Last Name").place(x=40, y=80)
+        self.first_entry = tk.Entry(root, textvariable = self.first_label, width=30).place(x=130,y=60)
+        self.last_entry = tk.Entry(root, textvariable = self.last_label, width=30).place(x=130, y=80)
+        self.age_entry = tk.Entry(root, textvariable = self.age_var, width=30).place(x=130,y=100)
+        self.grade_entry = tk.Entry(root, textvariable = self.grade_var, width=30).place(x=130,y=120)
 
-        self.button = tk.Button(root, text='Write to Database', command=self.write).place(x=75, y=130)
-
-        first_entry = tk.Entry(root, textvariable = self.first, width=30).place(x=110,y=60)
-
-        last_entry = tk.Entry(root, textvariable = self.last, width=30).place(x=110, y=80)
+        self.submite_button = tk.Button(root, text='Write to Database', command=self.write).place(x=160,y=170)
 
         root.mainloop()
 
     def write(self):
-        self.first_name = self.first.get()
-        self.last_name = self.last.get()
-        print("First Name: " + self.first_name)
-        print("Last Name: " + self.last_name)
-    #    con = pymysql.connect(host="localhost", user="root", password="ctu1234", database="batman")
-    #    cur = con.cursor()
-    #    cur.execute("insert into students values(%s, %s)", (self.id_var.get(), self.last.get()))
-    #    con.commit()
-    #    con.close()
-    #    messagebox.showinfo("Successfull", "Record has been inserted.")
+        test_database_build.cursor.execute('''
+        INSERT INTO student (student_ID, first_name, last_name, age, grade_level)
+        VALUES (%s, %s, %s, %s, %s)
+        ''', (self.id_var, self.first_var.get(), self.last_var.get(), self.age_var.get(), self.grade_var.get()))
+        test_database_build.mydb.commit()
 
 class studentModify:
     pass
